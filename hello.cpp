1948a5403570dc15268ec45ba7549f117d9d9f97
Keeper9 <jermaine.n.reid@gmail.com>
2019-08-31 18:25:22 -0400

    second code

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,4 @@
+//Global Scope
 //Name: Jermaine Reid
 //Assignment: hello.cpp
 //Date: 08/31/2019
@@ -8,8 +9,20 @@ on its own line*/
 #include <iostream>
 using namespace std;
 
+int a = 5;
+//Global Scope
 int main()
 {
+    //int a = 6;
+    cout << a;
+    //Local Scope
     cout << "Hello World ";
+    {
+        int a = 8;
+        //Local Scope Child
+        cout << "Hello People\n";
+        
+    }
     return 0;
 }
+//Global Scope
