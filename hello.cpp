0d7c2c7096f6722eb38ef06ad7f292cebe1441b6
Keeper9 <jermaine.n.reid@gmail.com>
2019-08-31 17:59:32 -0400

    first code

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+//Name: Jermaine Reid
+//Assignment: hello.cpp
+//Date: 08/31/2019
+/*Description: This program will
+display the message "Hello World"
+on its own line*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World ";
+    return 0;
+}
