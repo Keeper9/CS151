a29277354af03acd61bb414cb00ab7bb36d2fa26
Keeper9 <jermaine.n.reid@gmail.com>
2019-09-04 09:42:50 -0400

    2nd day Group A

--- hello.cpp
+++ hello.cpp
@@ -1,28 +1,11 @@
-//Global Scope
-//Name: Jermaine Reid
-//Assignment: hello.cpp
-//Date: 08/31/2019
-/*Description: This program will
-display the message "Hello World"
-on its own line*/
-
 #include <iostream>
 using namespace std;
 
-int a = 5;
-//Global Scope
 int main()
 {
-    //int a = 6;
-    cout << a;
-    //Local Scope
-    cout << "Hello World ";
-    {
-        int a = 8;
-        //Local Scope Child
-        cout << "Hello People\n";
-        
-    }
+    std::cout << "Hello World\n";
+    cout << 21;
+    cout << 3.1415;
+    cout << 21 + 32;
     return 0;
 }
-//Global Scope
